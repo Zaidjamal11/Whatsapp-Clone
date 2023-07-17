e743bef6cc431bef24e33cc654e09847cc47a5e2
unknown <zaidj777@gmail.com>
2023-07-18 00:38:26 +0530

    Google Authentication Added

--- client/src/components/account/LoginDialog.jsx
+++ client/src/components/account/LoginDialog.jsx
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
 
-import { Dialog , Box, Typography, List, ListItem, styled} from '@mui/material';
+import { Dialog, Box, Typography, List, ListItem, styled } from "@mui/material";
 
-import { qrCodeImage } from '../../constants/data';
+import { GoogleLogin } from "@react-oauth/google";
+
+import { qrCodeImage } from "../../constants/data";
+
+import jwt_decode from 'jwt-decode';
 
 const Component = styled(Box)`
   display: flex;
@@ -12,11 +16,10 @@ const Container = styled(Box)`
   padding: 56px 0 56px 56px;
 `;
 
-
-const QrCode = styled('img')({
-    height: 264,
-    width: 264,
-    margin: '50px 0 0 50px'
+const QrCode = styled("img")({
+  height: 264,
+  width: 264,
+  margin: "50px 0 0 50px",
 });
 
 const Title = styled(Typography)`
@@ -27,57 +30,73 @@ const Title = styled(Typography)`
   margin-bottom: 25px;
 `;
 
-
 const StyledList = styled(List)`
   & > li {
     padding: 0;
-    margin-top:15px;
+    margin-top: 15px;
     font-size: 18px;
     line-height: 28px;
-    color: #4a4a4a
-
-
+    color: #4a4a4a;
   }
-
-`
+`;
 
 const dialogStyle = {
-    height: '96%',
-    marginTop: '12%',
-    width: '60%',
-    maxWidth: '100%',
-    maxHeight: '100%',
-    boxShadow: 'none',
-    overflow: 'hidden'
-}
-
+  height: "96%",
+  marginTop: "12%",
+  width: "60%",
+  maxWidth: "100%",
+  maxHeight: "100%",
+  boxShadow: "none",
+  overflow: "hidden",
+};
 
 const LoginDialog = () => {
-  return (
-    <Dialog 
-         open = {true}
-         PaperProps={{ sx: dialogStyle}}
+
+
+  const onLoginSuccess = (res) => {
+
+    const decoded = jwt_decode(res.credential);
     
-    >
-        <Component>
-            <Container>
-                <Title>To use WhatsApp on your computer:</Title>
-                <StyledList>
-                    <ListItem> 1. Open WhatsApp on your phone </ListItem>
-                    <ListItem> 2. Tap Menu : or Settings and  select Linked Devices </ListItem>
-                    <ListItem> 3. Point your phone to this screen to capture the code </ListItem>
-                </StyledList>
+  };
 
-            </Container>
-                 <QrCode src={qrCodeImage} alt="qr-code" />
 
-            <Box>
 
-            </Box>
-        </Component>
+  const onLoginError = (res) => {
+    
+    console.log("Login Failed", res)
+  };
 
+  return (
+    <Dialog open={true} PaperProps={{ sx: dialogStyle }}>
+      <Component>
+        <Container>
+          <Title>To use WhatsApp on your computer:</Title>
+          <StyledList>
+            <ListItem> 1. Open WhatsApp on your phone </ListItem>
+            <ListItem>
+              {" "}
+              2. Tap Menu : or Settings and select Linked Devices{" "}
+            </ListItem>
+            <ListItem>
+              {" "}
+              3. Point your phone to this screen to capture the code{" "}
+            </ListItem>
+          </StyledList>
+        </Container>
+
+        <Box style={{ position: 'relative'}}>
+          <QrCode src={qrCodeImage} alt="qr-code" />
+          <Box style={{ position: 'absolute', top: '50%', transform: 'translateX(45%)'}}>
+              <GoogleLogin
+                 onSuccess={onLoginSuccess}
+                 onError={onLoginError}
+
+              />
+          </Box>
+        </Box>
+      </Component>
     </Dialog>
-  )
-}
+  );
+};
 
 export default LoginDialog;
