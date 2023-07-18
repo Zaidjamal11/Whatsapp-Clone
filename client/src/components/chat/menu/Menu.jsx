de8243a9a89825366788ab199841545fc234e608
unknown <zaidj777@gmail.com>
2023-07-18 21:12:25 +0530

    SearchField Addd

--- client/src/components/chat/menu/Menu.jsx
+++ client/src/components/chat/menu/Menu.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Box } from '@mui/material';
 
 
+
+
 //components
 import Header from './Header';
+import Search from './Search';
 
 const Menu = () => {
   return (
     <Box>
       <Header />
+      <Search />
     </Box>
   )
 }
