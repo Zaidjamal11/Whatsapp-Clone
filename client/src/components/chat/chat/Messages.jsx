ff202990929f261e3072f20c6757d4116dcd6a2c
unknown <zaidj777@gmail.com>
2023-07-21 01:27:30 +0530

    Messages Display on in the chatBox

--- client/src/components/chat/chat/Messages.jsx
+++ client/src/components/chat/chat/Messages.jsx
@@ -1,68 +1,76 @@
-import  { useContext , useState , useEffect} from 'react';
+import { useContext, useState, useEffect } from "react";
 
-import { AccountContext } from '../../../context/AccountProvider';
-import { Box, styled } from '@mui/material';
+import { AccountContext } from "../../../context/AccountProvider";
+import { Box, styled } from "@mui/material";
 
-import { getMessages, newMessage } from '../../../service/api';
+import { getMessages, newMessage } from "../../../service/api";
 
 //components
-import Footer from './Footer';
-
+import Footer from "./Footer";
+import Message from "./Message";
 
 const Wrapper = styled(Box)`
-  background-image: url(${'https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png'});
+  background-image: url(${"https://user-images.githubusercontent.com/15075759/28719144-86dc0f70-73b1-11e7-911d-60d70fcded21.png"});
   background-size: 50%;
-
 `;
 
 const Component = styled(Box)`
   height: 79vh;
   overflow-y: scroll;
+`;
+
+
+const Container = styled(Box)`
+   padding: 1px 80px;
 `
 
+const Messages = ({ person, conversation }) => {
+  
+  const [value, setValue] = useState("");
 
-const Messages = ({ person, conversation}) => {
+  const [messages, setMessages] = useState([]);
+
+  const [newMessageFlag , setNewMessageFlag] = useState(false);
 
   const { account } = useContext(AccountContext);
-  const [value, setValue] = useState('');
-
-
 
   useEffect(() => {
     const getMessageDetails = async () => {
       let data = await getMessages(conversation._id);
-      
-    }
+      setMessages(data);
+    };
     conversation._id && getMessageDetails();
-  },[person._id, conversation._id])
+  }, [person._id, conversation._id, newMessageFlag]);
 
-
-
-  const sendText = async(e) => {
+  const sendText = async (e) => {
     const code = e.keyCode || e.which;
-    if(code === 13){
+    if (code === 13) {
       let message = {
-        senderId : account.sub,
+        senderId: account.sub,
         receiverId: person.sub,
         conversationId: conversation._id,
-        type: 'text',
-        text: value
-
-      }
+        type: "text",
+        text: value,
+      };
       await newMessage(message);
-      setValue('');
+      setValue("");
+      setNewMessageFlag(prev => !prev);
+      
     }
-
-  }
+  };
 
   return (
     <Wrapper>
       <Component>
-
+        {messages && messages.map((message) => (
+            <Container>
+                <Message message={message} />
+            </Container>
+        ))}
       </Component>
       <Footer sendText={sendText} setValue={setValue} value={value} />
     </Wrapper>
-  )
-}
+  );
+};
 
 export default Messages;
