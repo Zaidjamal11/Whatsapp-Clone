43814c67fd36138e1e2ca1ab1a532f6256d8ab64
unknown <zaidj777@gmail.com>
2023-07-22 21:43:37 +0530

    Socket Server Established

--- client/src/context/AccountProvider.jsx
+++ client/src/context/AccountProvider.jsx
@@ -1,4 +1,7 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useRef, useEffect } from "react";
+
+import { io } from 'socket.io-client';
+
 
 export const AccountContext = createContext(null);
 
@@ -6,13 +9,20 @@ const AccountProvider = ({ children }) => {
   const [account, setAccount] = useState();
   const[person, setPerson] = useState({});
 
+  const socket = useRef();
+
+  useEffect(() => {
+    socket.current = io('ws://localhost:9000');
+}, [])
+
   return (
     <AccountContext.Provider
       value={{
         account,
         setAccount,
         person,
-        setPerson
+        setPerson,
+        socket,
 
       }}
     >
